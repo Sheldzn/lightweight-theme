b8e8fc3b3b6b02d37f5b84f335476e5fb77676e2
Sheldon <sjenkins.dev@gmail.com>
2026-02-12 13:23:29 +0200

    Create Cart Drawer

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,10 @@
  * Main theme JavaScript entry.
  * Add your scripts here or import components.
  */
+import { initCartDrawer } from "./components/cartDrawer.js"
+
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", initCartDrawer)
+} else {
+  initCartDrawer()
+}
